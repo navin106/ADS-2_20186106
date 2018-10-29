aa3d7b58ceea5a5471cb4b8caff7751493f83847
navin106 <sandula.navin@msitprogram.net>
2018-10-29 17:22:14 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/1.1 Graph/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
+interface Graph {
+	public int V();
+	public int E();
+	public void addEdge(int v, int w);
+	public Iterable<Integer> adj(int v);
+	public boolean hasEdge(int v, int w);
+}
+public class Solution {
+	private Solution() {
+		Scanner sc = new Scanner(System.in);
+		String dc = sc.nextLine();
+		int vertices = sc.nextInt();
+		int edges = sc.nextInt();
+		String[] keys = sc.nextLine().split(",");
+		System.out.println(Arrays.toString(keys));
+		/*for (int i = 0; i < edges; i++) {
+
+
+		}*/
+	}
+}
+
