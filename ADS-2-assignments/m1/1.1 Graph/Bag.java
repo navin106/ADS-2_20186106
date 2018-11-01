20cefb5334093409167d819dba036b16f6fbdd02
navin106 <sandula.navin@msitprogram.net>
2018-11-01 17:00:12 +0530

    "ads2_m1_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS-2-assignments/m1/1.1 Graph/Bag.java
@@ -0,0 +1,134 @@
+/*************************************************************************
+ *  Compilation:  javac Bag.java
+ *  Execution:    java Bag < input.txt
+ *
+ *  A generic bag or multiset, implemented using a linked list.
+ *
+ *************************************************************************/
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * The <tt>Bag</tt> class represents a bag (or multiset) of generic items. It
+ * supports insertion and iterating over the items in arbitrary order. <p> The
+ * <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation take constant
+ * time. Iteration takes time proportional to the number of items. <p> For
+ * additional documentation, see <a
+ * href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ * @param      <Item>  The item
+ */
+public class Bag<Item> implements Iterable<Item> {
+    /**
+     * { var_description }.
+     */
+    private int n;         // number of elements in bag
+    /**
+     * { var_description }.
+     */
+    private Node first;    // beginning of bag
+    /**
+     * Class for node.
+     */
+    private class Node {
+        /**
+         * { var_description }.
+         */
+        private Item item;
+        /**
+         * { var_description }.
+         */
+        private Node next;
+    }
+
+    /**
+      * Create an empty stack.
+      */
+    public Bag() {
+        first = null;
+        n = 0;
+    }
+
+    /**
+      * Is the BAG empty?
+      *
+      * @return     True if empty, False otherwise.
+      */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+      * Return the number of items in the bag.
+      *
+      * @return     { description_of_the_return_value }
+      */
+    public int size() {
+        return n;
+    }
+
+    /**
+      * Add the item to the bag.
+      *
+      * @param      item  The item
+      */
+    public void add(final Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        n++;
+    }
+
+
+    /**
+      * Return an iterator that iterates over the items in the bag.
+      *
+      * @return     { description_of_the_return_value }
+      */
+    public Iterator<Item> iterator()  {
+        return new ListIterator();
+    }
+    /**
+     * Class for list iterator.
+     */
+    private class ListIterator implements Iterator<Item> {
+        /**
+         * { var_description }.
+         */
+        private Node current = first;
+
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext()  {
+            return current != null;
+        }
+
+        /**
+         * { function_description }.
+         */
+        public void remove()      {
+            throw new UnsupportedOperationException();
+        }
+
+        /**
+         * { function_description }.
+         *
+         * @return     { description_of_the_return_value }
+         */
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+}
---
+++ ADS-2-assignments/m1/1.1 Graph/Solution.java
@@ -0,0 +1,307 @@
+import java.util.Scanner;
+/**
+ * Interface for graph.
+ */
+interface Graph {
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int ver();
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int ed();
+    /**
+     * Adds an edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
+    void addEdge(int v, int w);
+    /**
+     * { function_description }.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    Iterable<Integer> adj(int v);
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    boolean hasEdge(int v, int w);
+}
+/**
+ * List of graphs.
+ */
+class GraphList implements Graph {
+    /**
+     * vertices.
+     */
+    private int vertices;
+    /**
+     * edges.
+     */
+    private int edge;
+    /**
+     * bag.
+     */
+    private Bag<Integer>[] adj;
+    /**
+     * Constructs the object.
+     *
+     * @param      v1    The v 1
+     */
+    GraphList(final int v1) {
+        this.vertices = v1;
+        this.edge = 0;
+        this.adj = (Bag<Integer>[]) new Bag[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adj[i] = new Bag<Integer>();
+        }
+    }
+    /**
+     * vertices.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int ver() {
+        return this.vertices;
+    }
+    /**
+     * number of edges.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int ed() {
+        return this.edge;
+    }
+    /**
+     * Adds an edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
+    public void addEdge(final int v, final int w) {
+        if (!hasEdge(v, w) && v != w) {
+            edge++;
+        }
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+    /**
+     * iterable.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Integer> adj(final int v) {
+        return adj[v];
+    }
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(final int v, final int w) {
+        int count = 0;
+        for (int i : adj[v]) {
+            if (i == w) {
+                count += 1;
+                break;
+            }
+        }
+        for (int i : adj[w]) {
+            if (i == v) {
+                count += 1;
+                break;
+            }
+        }
+        if (count == 2) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * display.
+     *
+     * @param      data  The data
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public String display(final String[] data) {
+        String s = "";
+        s += vertices + " vertices, " + edge + " edges" + '\n';
+        if (edge == 0) {
+            s += "No edges ";
+        } else {
+            for (int v = 0; v < vertices; v++) {
+                s += data[v] + ": ";
+                for (int w : adj[v]) {
+                    s += data[w] + " ";
+                }
+                s += '\n';
+            }
+        }
+        return s.substring(0, s.length() - 1);
+    }
+
+}
+/**
+ * Class for graph matrix.
+ */
+class GraphMatrix implements Graph {
+    /**
+     * number of vertices.
+     */
+    private int vertices;
+    /**
+     * number of edges.
+     */
+    private int edges;
+    /**
+     * matrix.
+     */
+    private int[][] matrix;
+    /**
+     * Constructs the object.
+     *
+     * @param      v1    The v1.
+     */
+    GraphMatrix(final int v1) {
+        this.vertices = v1;
+        this.edges = 0;
+        this.matrix = new int[vertices][vertices];
+        for (int i = 0; i < vertices; i++) {
+            for (int j = 0; j < vertices; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    /**
+     * Iterable.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Integer> adj(final int v) {
+        return null;
+    }
+    /**
+     * number of vertices.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int ver() {
+        return this.vertices;
+    }
+    /**
+     * number of edges.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int ed() {
+        return this.edges;
+    }
+    /**
+     * Adds an edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
+    public void addEdge(final int v, final int w) {
+        if (!hasEdge(v, w) && v != w) {
+            edges++;
+        }
+        matrix[v][w] = 1;
+        matrix[w][v] = 1;
+    }
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     *
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(final int v, final int w) {
+        return (matrix[v][w] == 1);
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String s = "";
+        s += vertices + " vertices, " + edges + " edges" + '\n';
+        if (edges == 0) {
+            s += "No edges ";
+        } else {
+            for (int i = 0; i < vertices; i++) {
+                for (int j = 0; j < vertices; j++) {
+                    s += matrix[i][j] + " ";
+                }
+                s = s.substring(0, s.length());
+                s += ('\n');
+            }
+        }
+        return s.substring(0, s.length() -  1);
+    }
+}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * constructor.
+     */
+    private Solution() {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String dc = sc.nextLine();
+        int vertices = sc.nextInt();
+        int edges = sc.nextInt();
+        sc.nextLine();
+        String[] keys = sc.nextLine().split(",");
+        if (dc.equals("List")) {
+            GraphList list = new GraphList(vertices);
+            for (int i = 0; i < edges; i++) {
+                String[] edgeInp = sc.nextLine().split(" ");
+                list.addEdge(Integer.parseInt(edgeInp[0]),
+                             Integer.parseInt(edgeInp[1]));
+            }
+            System.out.println(list.display(keys));
+        }
+        if (dc.equals("Matrix")) {
+            GraphMatrix mat = new GraphMatrix(vertices);
+            for (int i = 0; i < edges; i++) {
+                String[] edgeInp = sc.nextLine().split(" ");
+                mat.addEdge(Integer.parseInt(edgeInp[0]),
+                             Integer.parseInt(edgeInp[1]));
+            }
+            System.out.println(mat);
+        }
+    }
+}
+
