6fee393aab8ed00538fd7e02c1ad498eb6066090
navin106 <sandula.navin@msitprogram.net>
2018-11-03 11:54:56 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m6/ADS-2Week-1/Solution.java
+++ ADS-2-assignments/m6/ADS-2Week-1/Solution.java
@@ -13,10 +13,9 @@ class PageRank {
 	double getPR(int v) {
 		double pr = pray[v];
 		for (int j = 0; j < 1000; j++) {
-				for (int i : digr.adj(v)) {
-					pr += pr / digr.outdegree(i);
-				}
-			System.out.println("Mul");
+			for (int i : digr.adj(v)) {
+				pr += pr / digr.outdegree(i);
+			}
 		}
 		return pr;
 	}
