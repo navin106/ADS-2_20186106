ad32fe027a381565cc49bd53c40715145de657ac
navin106 <sandula.navin@msitprogram.net>
2018-11-09 11:08:31 +0530

    "ads2_m10_a1_1: 0/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m10/Seam Carving/SeamCarver.java
+++ ADS-2-assignments/m10/Seam Carving/SeamCarver.java
@@ -1,7 +1,72 @@
 
 public class SeamCarver {
 	// create a seam carver object based on the given picture
-	public SeamCarver(Picture picture) {
+	private Picture pic;
+	private int width;
+	private int height;
+	private double[][] energy;
+	private double top = 0.0;
+	private double bottom = 0.0;
+	public double[][] getenergy() {
+		return energy;
+	}
+
+	public void setenergy(double[][] energy) {
+		this.energy = energy;
+	}
+	public double gettop() {
+		return top;
+	}
+
+	public void settop(double top) {
+		this.top = top;
+	}
+	public double  getbottom() {
+		return bottom;
+	}
+
+	public void setbottom(double  bottom) {
+		this.bottom = bottom;
+	}
+	public int getheight() {
+		return height;
+	}
+
+	public void setheight(int height) {
+		this.height = height;
+	}
+
+	public Picture getpic() {
+		return pic;
+	}
+
+	public void setpic(Picture pic) {
+		this.pic = pic;
+	}
+	public int getwidth() {
+		return width;
+	}
+
+	public void setwidth(int width) {
+		this.width = width;
+	}
+	public SeamCarver() {
+		
+	}
+	public SeamCarver(Picture picture) throws Exception {
+		this.pic = picture;
+		this.width = picture.width();
+		this.height = picture.height();
+		energy = new double[height][width];
+		if (pic == null) {
+			throw new Exception("picture is null");
+		}
+		for (int i = 0; i < height; i++) {
+			for (int j=0; j<width; j++) {
+				energy[i][j] = energy(i, j);	
+			}
+
+		}
 
 	}
 	// current picture
