d1e27c62118b0ed74c33b2d86ce8fc13163224c6
navin106 <sandula.navin@msitprogram.net>
2018-11-10 10:30:45 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -1,14 +1,43 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
 
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		// Self loops are not allowed...
 		// Parallel Edges are allowed...
-		// Take the Graph input here...
-
-		String caseToGo = null;
+		// Take the Graph input here..
+		Scanner sc = new Scanner(System.in);
+		// String caseToGo = null;
+		int cities = sc.nextInt();
+		int paths = sc.nextInt();
+		sc.nextLine();
+		EdgeWeightedGraph graph = new EdgeWeightedGraph(cities);
+		for (int i = 0; i < paths; i++) {
+			String[] distPath = sc.nextLine().split(" ");
+			Edge e = new Edge(Integer.parseInt(distPath[0]),
+			                  Integer.parseInt(distPath[0]),
+			                  Integer.parseInt(distPath[0]));
+			graph.addEdge(e);
+		}
+		String caseToGo = sc.nextLine();
 		switch (caseToGo) {
 		case "Graph":
 			//Print the Graph Object.
+			System.out.println(graph);
 			break;
 
 		case "DirectedPaths":
