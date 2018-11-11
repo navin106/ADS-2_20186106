222ca9714cb1d4e5db8c58f91d866ff8062083af
navin106 <sandula.navin@msitprogram.net>
2018-11-11 15:48:33 +0530

    "ads2_m4_a1_1: 14/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -0,0 +1,63 @@
+/**
+ * Files import.
+ */
+import java.io.File;
+/**
+ * Scanner import.
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Empty Constructor.
+    }
+    /**
+     * Main function.
+     *
+     * @param      args       The arguments
+     *
+     * @throws     Exception  { exception_description }
+     */
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String synsets = sc.nextLine();
+        String hypernyms = sc.nextLine();
+        try {
+            WordNet wrdnet = new WordNet(synsets, hypernyms);
+            String input = sc.nextLine();
+            if (wrdnet.gethasCycle()) {
+                System.out.println("Cycle detected");
+                return;
+            }
+
+            if (input.equals("Graph")) {
+                wrdnet.checkMultipleRoots();
+                if (wrdnet.gethasMultipleRoots()) {
+                    return;
+                } else {
+                    System.out.println(wrdnet.getDigraph());
+                }
+            } 
+            if (input.equals("Queries")) {
+                while (sc.hasNextLine()) {
+                    String[] tokens = sc.nextLine().split(" ");
+                    try {
+                        wrdnet.sap(tokens[0], tokens[1]);
+                        System.out.println("distance = "
+                            + wrdnet.distance(tokens[0], tokens[1])
+                            + ", ancestor = " + wrdnet.sap(tokens[0], tokens[1]));
+                    } catch (Exception e) {
+                        System.out.println("IllegalArgumentException");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
