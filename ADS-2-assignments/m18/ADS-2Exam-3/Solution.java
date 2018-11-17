68f0eafc4b1a13ec5ff0fc3a817327cf31d18714
navin106 <sandula.navin@msitprogram.net>
2018-11-17 10:39:32 +0530

    "ads2_m18_a1_1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m18/ADS-2Exam-3/Solution.java
+++ ADS-2-assignments/m18/ADS-2Exam-3/Solution.java
@@ -90,14 +90,14 @@ public class Solution {
 		// your code goes here
 		In in = new In(file);
 		String[] dictionary = in.readAllStrings();
-		for (int i = 0; i <= dictionary.length; i++) {
+		for (int i = 0; i < dictionary.length; i++) {
 			if (st.get(dictionary[i]) == null) {
 				st.put(dictionary[i], 1);
-			} else {
-				st.put(dictionary[i],st.get(dictionary[i])+1);
 			}
+			st.put(dictionary[i], st.get(dictionary[i]) + 1);
 
-			}
+
+		}
 		return st;
 	}
 
