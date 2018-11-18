6883f47572fb13e3792b7a003a273995ed8f6603
navin106 <sandula.navin@msitprogram.net>
2018-11-18 19:54:31 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m13/Radix Sorts/LSD.java
+++ ADS-2-assignments/m13/Radix Sorts/LSD.java
@@ -4,49 +4,49 @@ import java.util.Arrays;
  */
 public class LSD {
 
-	/**
-	 * Constructs the object.
-	 */
-	public LSD() {
+    /**
+     * Constructs the object.
+     */
+    protected LSD() {
 
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      a     { parameter_description }
-	 * @param      w     { parameter_description }
-	 */
-	public static void sort(final String[] a, final int w) {
-		int n = a.length;
-		final int radix = 256;   // extend ASCII alphabet size
-		String[] aux = new String[n];
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      a     { parameter_description }
+     * @param      w     { parameter_description }
+     */
+    public static void sort(final String[] a, final int w) {
+        int n = a.length;
+        final int radix = 256;   // extend ASCII alphabet size
+        String[] aux = new String[n];
 
-		for (int d = w - 1; d >= 0; d--) {
-			// sort by key-indexed counting on dth character
+        for (int d = w - 1; d >= 0; d--) {
+            // sort by key-indexed counting on dth character
 
-			// compute frequency counts
-			int[] count = new int[radix + 1];
-			for (int i = 0; i < n; i++) {
-				// System.out.println(a[i].charAt(d));
-				count[a[i].charAt(d) + 1]++;
-				// System.out.println(Arrays.toString(count));
-			}
+            // compute frequency counts
+            int[] count = new int[radix + 1];
+            for (int i = 0; i < n; i++) {
+                // System.out.println(a[i].charAt(d));
+                count[a[i].charAt(d) + 1]++;
+                // System.out.println(Arrays.toString(count));
+            }
 
-			// compute cumulates
-			for (int r = 0; r < radix; r++) {
-				count[r + 1] += count[r];
-			}
+            // compute cumulates
+            for (int r = 0; r < radix; r++) {
+                count[r + 1] += count[r];
+            }
 
-			// move data
-			for (int i = 0; i < n; i++) {
-				aux[count[a[i].charAt(d)]++] = a[i];
-			}
+            // move data
+            for (int i = 0; i < n; i++) {
+                aux[count[a[i].charAt(d)]++] = a[i];
+            }
 
-			// copy back
-			for (int i = 0; i < n; i++) {
-				a[i] = aux[i];
-			}
-		}
-		System.out.println(Arrays.toString(a));
-	}
+            // copy back
+            for (int i = 0; i < n; i++) {
+                a[i] = aux[i];
+            }
+        }
+        System.out.println(Arrays.toString(a));
+    }
 }
--- ADS-2-assignments/m13/Radix Sorts/Solution.java
+++ ADS-2-assignments/m13/Radix Sorts/Solution.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
  * sloution class.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		LSD lsd = new LSD();
-		int k = 0;
-		int n = Integer.parseInt(sc.nextLine());
-		String[] words = new String[n];
-		for (int i = 0; i < n; i++) {
-			words[i] = sc.nextLine();
-			k = words[i].length();
-		}
-		lsd.sort(words, k);
-	}
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LSD lsd = new LSD();
+        int k = 0;
+        int n = Integer.parseInt(sc.nextLine());
+        String[] words = new String[n];
+        for (int i = 0; i < n; i++) {
+            words[i] = sc.nextLine();
+            k = words[i].length();
+        }
+        lsd.sort(words, k);
+    }
 }
